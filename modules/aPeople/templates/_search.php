a06fa144583a001ddfdaf77200efb283bf310296
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-02-10 20:17:54 +0000

    search
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3339 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aPeople/templates/_search.php
+++ modules/aPeople/templates/_search.php
@@ -1,4 +1,6 @@
 <ul class="people-search-results">
+	<h3>Faculty &amp; People</h3>
+	<br />
   <?php foreach ($results as $person): ?>
     <li><?php echo link_to($person, 'aPeople/show?' . http_build_query(array('slug' => $person->slug))) ?></li>
   <?php endforeach ?>
