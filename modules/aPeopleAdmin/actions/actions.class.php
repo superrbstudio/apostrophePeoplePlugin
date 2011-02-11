ead44c825b6f29d0a83b6037aedc66d31c68b2ac
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-02-11 19:35:12 +0000

    made the people admin actions easier to override
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3344 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aPeopleAdmin/actions/actions.class.php
+++ modules/aPeopleAdmin/actions/actions.class.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/aPeopleAdminGeneratorConfiguration.class.php';
-require_once dirname(__FILE__).'/../lib/aPeopleAdminGeneratorHelper.class.php';
-
 /**
  * person actions.
  *
@@ -11,43 +8,7 @@ require_once dirname(__FILE__).'/../lib/aPeopleAdminGeneratorHelper.class.php';
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
  */
-class aPeopleAdminActions extends autoAPeopleAdminActions
+class aPeopleAdminActions extends PluginaPeopleAdminActions
 {
-  public function executeHeadshot(sfWebRequest $request)
-  {
-    $person = Doctrine::getTable('aPerson')->find($request->getParameter('id'));
 
-    if (!$request->hasParameter('aMediaId') && !$request->getParameter('aMediaCancel'))
-    {
-      $url = 'aMedia/select?' .
-        http_build_query(array(
-          'engine-slug' => '/admin/media',
-          'multiple' => false,
-          'label' => 'Choose a person headshot', 
-          'aMediaId' => $person->getHeadshotId(),
-          'type' => 'image',
-          'minimum-width' => 150,
-          'minimum-height' => 200,
-          'after' => $this->getController()->genUrl('aPeopleAdmin/headshot?id='.$person->getId())
-        ));
-
-      return $this->executeMedia($request, $url);
-    }
-    elseif (!$request->getParameter('aMediaCancel'))
-    {
-      $person->setHeadshotId($request->getParameter('aMediaId'));
-      $person->save();
-      
-      return $this->redirect('aPeopleAdmin/index');
-    }
-
-    return $this->redirect('aPeopleAdmin/index');
-  }
-  
-  
-  public function executeMedia(sfWebRequest $request, $url)
-  {    
-    $url = $this->getController()->genUrl($url);
-    return $this->redirect($url);
-  }
 }
