b47f785baa0bc0236a9f2556c38aeede71afc01d
thinkswan <thinkswan@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-06-17 17:16:41 +0000

    Removed created_at, updated_at, and slug fields from People plugin new/edit views
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3764 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/doctrine/PluginaPersonForm.class.php
+++ lib/form/doctrine/PluginaPersonForm.class.php
@@ -15,11 +15,14 @@ abstract class PluginaPersonForm extends BaseaPersonForm
 		parent::setup();
 		$this->widgetSchema['body']    = new aWidgetFormRichTextarea(array('editor' => 'fck', 'height' => '300', 'width' => '500'));
 		$this->validatorSchema['body'] = new sfValidatorHtml(array('required' => false));
-		$this->widgetSchema['created_at']    = new awidgetformjquerydate(array('image' => '/apostrophePlugin/images/a-icon-datepicker.png'));
-		$this->validatorSchema['created_at'] = new sfValidatorDate(array('required' => false));
-		$this->widgetSchema['updated_at']    = new awidgetformjquerydate(array('image' => '/apostrophePlugin/images/a-icon-datepicker.png'));
-		$this->validatorSchema['updated_at'] = new sfValidatorDate(array('required' => false));
-		unset($this['headshot_id']);
+		$this->widgetSchema['sex']     = new sfWidgetFormChoice(array('choices' => array('' => '', 'Male' => 'Male', 'Female' => 'Female')));
+		$this->validatorSchema['sex']  = new sfValidatorChoice(array('choices' => array(0 => '', 1 => 'Male', 2 => 'Female'), 'required' => false));
+		unset(
+		  $this['headshot_id'],
+		  $this['created_at'],
+		  $this['updated_at'],
+		  $this['slug']
+		);
 
 		// Alphabetize the Categories dropdown 
    	$this->getWidget('categories_list')->setOption('query', Doctrine::getTable('aCategory')->createQuery()->orderBy('aCategory.name asc'));	
