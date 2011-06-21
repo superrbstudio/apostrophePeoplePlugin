9fb4615c71dec2ad55a1d1fc17b8f3df857813eb
wjohnald <wjohnald@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-06-21 13:56:54 +0000

    Plugin-ified aPeopleCategoryForm
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3781 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ lib/form/PluginaPeopleCategoryForm.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class PluginaPeopleCategoryForm extends BaseForm
+{
+  public function  configure() {
+    parent::configure();
+
+
+    $categories = Doctrine_Query::create()
+      ->from('aCategory INDEXBY id')
+      ->select('name')
+      ->fetchArray();
+
+    $choices = array();
+
+    foreach($categories as $id => $category)
+    {
+      $choices[$id] = $category['name'];
+    }
+
+    $this->widgetSchema['categories'] = new sfWidgetFormChoice(array('multiple' => true, 'choices' => $choices));
+    $this->validatorSchema['categories'] = new sfValidatorChoice(array('choices' => array_keys($choices)));
+
+    $this->widgetSchema->setNameFormat('aPeopleCategoryFilter[%s]');
+  }
+}
--- lib/form/aPeopleCategoryForm.class.php
+++ lib/form/aPeopleCategoryForm.class.php
@@ -1,26 +1,5 @@
 <?php
 
-class aPeopleCategoryForm extends BaseForm
+class aPeopleCategoryForm extends PluginaPeopleCategoryForm
 {
-  public function  configure() {
-    parent::configure();
-
-
-    $categories = Doctrine_Query::create()
-      ->from('aCategory INDEXBY id')
-      ->select('name')
-      ->fetchArray();
-
-    $choices = array();
-
-    foreach($categories as $id => $category)
-    {
-      $choices[$id] = $category['name'];
-    }
-
-    $this->widgetSchema['categories'] = new sfWidgetFormChoice(array('multiple' => true, 'choices' => $choices));
-    $this->validatorSchema['categories'] = new sfValidatorChoice(array('choices' => array_keys($choices)));
-
-    $this->widgetSchema->setNameFormat('aPeopleCategoryFilter[%s]');
-  }
 }
