8ec6ebcdec374f7e1e8d39621ce8aa004468658c
thinkswan <thinkswan@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-01 18:35:34 +0000

    Added form validation to new person form (now requires first_name, last_name, proper email; link must be proper url, postal_code must be a 5-digit int)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3843 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/doctrine/PluginaPersonForm.class.php
+++ lib/form/doctrine/PluginaPersonForm.class.php
@@ -13,19 +13,90 @@ abstract class PluginaPersonForm extends BaseaPersonForm
 	public function setup()
 	{
 		parent::setup();
-		$this->widgetSchema['body']    = new aWidgetFormRichTextarea(array('editor' => 'fck', 'height' => '300', 'width' => '500'));
-		$this->validatorSchema['body'] = new sfValidatorHtml(array('required' => false));
-		$this->widgetSchema['sex']     = new sfWidgetFormChoice(array('choices' => array('' => '', 'M' => 'Male', 'F' => 'Female')));
-		$this->validatorSchema['sex']  = new sfValidatorChoice(array('choices' => array(0 => '', 1 => 'M', 2 => 'F'), 'required' => false));
-		unset(
-		  $this['headshot_id'],
-		  $this['created_at'],
-		  $this['updated_at'],
-		  $this['slug']
-		);
 
-		// Alphabetize the Categories dropdown 
-   	$this->getWidget('categories_list')->setOption('query', Doctrine::getTable('aCategory')->createQuery()->orderBy('aCategory.name asc'));	
+		$this->unsetFields();
+
+		$this->setWidget('body', new aWidgetFormRichTextarea(array('editor' => 'fck', 'height' => '300', 'width' => '500')));
+		$this->setWidget('sex', new sfWidgetFormChoice(array('choices' => array('' => '', 'M' => 'Male', 'F' => 'Female'))));
+		$this->getWidget('categories_list')->setOption('query', Doctrine::getTable('aCategory')->createQuery()->orderBy('aCategory.name asc')); // Alphabetize categories dropdown
+
+		$this->validatorSchema['body'] = new sfValidatorHtml(array('required' => false));
+		$this->validatorSchema['sex']  = new sfValidatorChoice(array('choices' => array(0 => '', 1 => 'M', 2 => 'F'), 'required' => false));
+
+   	// Set labels for form elements
+    $this->widgetSchema->setLabels(array(
+      'first_name' => 'First Name:',
+      'middle_name' => 'Middle Name:',
+      'last_name' => 'Last Name:',
+      'salutation' => 'Salutation:',
+      'suffix' => 'Suffix:',
+      'email' => 'Email Address:',
+      'address_1' => 'Address (line 1):',
+      'address_2' => 'Address (line 2):',
+      'city' => 'City:',
+      'state' => 'State:',
+      'postal_code' => 'Zipcode:',
+      'work_phone' => 'Phone Number:',
+      'work_fax' => 'Fax Number:',
+      'body' => 'About this Person:',
+      'link' => 'Website:',
+      'sex' => 'Gender:',
+      'ethnicity' => 'Ethnicity:'
+    ));
+
+    // Set error messages for form elements
+    $this->setValidator('first_name', new sfValidatorString(
+      array('max_length' => 255, 'required' => true, 'trim' => true),
+      array(
+        'max_length' => 'Your first name must be shorter than %max_length% characters.',
+        'required' => 'You must provide your first name.'
+      )
+    ));
+    $this->setValidator('last_name', new sfValidatorString(
+      array('max_length' => 255, 'required' => true, 'trim' => true),
+      array(
+        'max_length' => 'Your last name must be shorter than %max_length% characters.',
+        'required' => 'You must provide your last name.'
+      )
+    ));
+    $this->setValidator('email', new sfValidatorEmail(
+      array('max_length' => 255, 'required' => true, 'trim' => true),
+      array(
+        'max_length' => 'Your email address must be shorter than %max_length% characters.',
+        'required' => 'You must provide your email address.',
+        'invalid' => 'Your email address must be of the form name@domain.ext'
+      )
+    ));
+    $this->setValidator('postal_code', new sfValidatorInteger(
+      array('max' => 99999, 'min' => 00000, 'required' => false, 'trim' => true),
+      array(
+        'max' => 'Your zip code must be 5 digits long.',
+        'min' => 'Your zip code must be 5 digits long.',
+        'invalid' => 'Your zip code must be composed of 5 digits.'
+      )
+    ));
+    $this->setValidator('link', new sfValidatorUrl(
+      array('max_length' => 1024, 'required' => false, 'trim' => true),
+      array(
+        'max_length' => 'Your website URL must be shorter than %max_length% characters.',
+        'invalid' => 'Your website URL must be of the form http://yourwebsite.ext'
+      )
+    ));
+    $this->setValidator('sex', new sfValidatorChoice(
+      array('choices' => array(0 => '', 1 => 'M', 2 => 'F'), 'required' => false),
+      array(
+        'invalid' => 'Please specify a valid gender by selecting an option from the dropdown menu.'
+      )
+    ));
 	}
-	
+
+	protected function unsetFields()
+	{
+	  // Don't show the following fields to the user
+    unset(
+      $this['headshot_id'], $this['created_at'],
+      $this['updated_at'], $this['slug']
+    );
+	}
+
 }
