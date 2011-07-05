bdf714fba857b8e5fd34cc900abc7f120d00d1b7
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-05 19:26:21 +0000

    look for the Base... class not the Plugin class for the actions
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3865 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aPeople/actions/actions.class.php
+++ modules/aPeople/actions/actions.class.php
@@ -8,6 +8,6 @@
  * @author     P'unk Avenue
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class aPeopleActions extends PluginaPeopleActions
+class aPeopleActions extends BaseaPeopleActions
 {
 }
--- modules/aPeople/actions/components.class.php
+++ modules/aPeople/actions/components.class.php
@@ -8,6 +8,6 @@
  * @author     P'unk Avenue
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class aPeopleComponents extends PluginaPeopleComponents
+class aPeopleComponents extends BaseaPeopleComponents
 {
 }
