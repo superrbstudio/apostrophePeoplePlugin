b089ad977dedd7fc1d9c72c388413e21d4f169fa
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-05 20:54:21 +0000

    annotated _person.php
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3874 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aPeople/templates/_person.php
+++ modules/aPeople/templates/_person.php
@@ -2,8 +2,23 @@
   <h4 class="name clearfix">
     <a class="person-expand-toggle" href="<?php echo url_for('aPeople_showPreview', array('slug' => $person->slug)) ?>" onclick="return false"><?php echo $person->getNameAndSuffix() ?></a>
   </h4>
+
   <?php /* Any additional information to be displayed in the list view goes here ?>
+
+		<div class="person-image clearfix">
+		  <?php if (!is_null($person->headshot_id)): ?>
+		    <?php include_component('aSlideshowSlot', 'slideshow', array(
+			  	'items' => array($person->Headshot),
+			  	'id' => $person->id.'-headshot',
+			  	'options' => array('width' => 50, 'height' => 50, 'resizeType' => 'c', 'arrows' => false)
+			  )) ?>
+			<?php endif ?>
+		</div>
+
 	  <div class="person-info clearfix"></div>
-	  <div class="person-info-expanded clearfix"></div>
+  	<div class="person-info-expanded clearfix">
+			<?php // Do Not Remove :: Ajax outputs HTML to this container ?>
+		</div>
 	<?php //*/ ?>
+
 </div>
