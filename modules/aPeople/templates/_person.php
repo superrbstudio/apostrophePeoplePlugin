7d7d43d4e69ab3af59de30da919fa8ad8ba4b5a1
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-05 20:25:47 +0000

    abstracted person out of the people partial
    the people partial is just a foreach loop
    
    person is the individual person markup
    
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePeoplePlugin/trunk@3868 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ modules/aPeople/templates/_person.php
@@ -0,0 +1,12 @@
+<div id="person-<?php echo $person->id ?>" class="person clearfix">
+  <h4 class="name clearfix">
+    <a class="person-expand-toggle" href="<?php echo url_for('aPeople_showPreview', array('slug' => $person->slug)) ?>" onclick="return false"><?php echo $person->getNameAndSuffix() ?></a>
+  </h4>
+
+  <?php // Any additional information to be displayed in the list view goes here ?>
+  <div class="person-info clearfix">
+  </div>
+  
+  <div class="person-info-expanded clearfix">
+  </div>
+</div>
